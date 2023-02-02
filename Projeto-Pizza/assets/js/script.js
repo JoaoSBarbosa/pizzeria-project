ada82ed3e2a70092404af655c1ed5c8c7893923d
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-02 20:15:01 -0300

    Definição inicial do modal

--- Projeto-Pizza/assets/js/script.js
+++ Projeto-Pizza/assets/js/script.js
@@ -1,5 +1,5 @@
 const c = (elemento) => document.querySelector(elemento);
-
+ 
 // mapeando o objeto
 pizzaJson.map((item, index) => {
   // clonando a estrutura pizza-item
@@ -11,6 +11,16 @@ pizzaJson.map((item, index) => {
   p(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
   p(".pizza-item--name").innerHTML = item.name;
   p('.pizza-item--desc').innerHTML = item.description;
+  // Adicionando evento do modal na tag a
+  p('a').addEventListener('click',(evento)=>{
+   evento.preventDefault(); // prevenindo o evento padrão
+   c('.pizzaWindowArea').style.opacity = 0;
+   c('.pizzaWindowArea').style.display = 'flex'
+   setTimeout(()=>{
+      c('.pizzaWindowArea').style.opacity = 1;
+   }, 200);
+
+  });
 
   //inserindo a estrutura clonada no area
   c(".pizza-area").append(pizzaItem);
