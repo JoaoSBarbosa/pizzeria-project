26f9a5f82cda32c541ef98e3bfb97fc9bb4f6776
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-02 20:00:12 -0300

    Adicionando os dados na estrutura via JavaScript

--- Projeto-Pizza/assets/js/script.js
+++ Projeto-Pizza/assets/js/script.js
@@ -1,8 +1,17 @@
 const c = (elemento) => document.querySelector(elemento);
+
 // mapeando o objeto
-pizzaJson.map((item, index)=>{
-   // clonando a estrutura pizza-item
-   let pizzaItem = c('.models .pizza-item').cloneNode(true);
-   //inserindo a estrutura clonada no area
-   c('.pizza-area').append(pizzaItem)
-})
+pizzaJson.map((item, index) => {
+  // clonando a estrutura pizza-item
+  let pizzaItem = c(".models .pizza-item").cloneNode(true);
+
+  const p = (add) =>pizzaItem.querySelector(add);
+// Adicionando os dados na estrutura
+  p(".pizza-item--img img").src = item.img;
+  p(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
+  p(".pizza-item--name").innerHTML = item.name;
+  p('.pizza-item--desc').innerHTML = item.description;
+
+  //inserindo a estrutura clonada no area
+  c(".pizza-area").append(pizzaItem);
+});
